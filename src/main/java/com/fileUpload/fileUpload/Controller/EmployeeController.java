06b66260929f6d74c597b37c4d2645607d74e61e
Jitendra Kumar Sahu <jitendra.kumar@in.accemy.com>
2019-02-05 12:20:31 +0000

    Add Update or GetAll API added

--- src/main/java/com/fileUpload/fileUpload/Controller/EmployeeController.java
+++ src/main/java/com/fileUpload/fileUpload/Controller/EmployeeController.java
@@ -3,12 +3,14 @@ package com.fileUpload.fileUpload.Controller;
 import com.fileUpload.fileUpload.Model.Employee;
 import com.fileUpload.fileUpload.Repository.EmployeeRepository;
 import com.fileUpload.fileUpload.Utils.FileStorageService;
+import com.fileUpload.fileUpload.Utils.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.Date;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/employees")// It is a base ULR
@@ -21,6 +23,7 @@ public class EmployeeController {
     @Autowired
     FileStorageService fileStorageService;
 
+    // Add a file in database API
     @PostMapping(path = "/add")
     public @ResponseBody
     Employee addNewEmployee(@RequestParam String name,
@@ -32,7 +35,7 @@ public class EmployeeController {
         if (file != null) {
             String fileName = fileStorageService.storeMultipartFile(file, FILE_SERVICE_STORAGE_DIRECTORY, employee.getId() + "");
             String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                    .path("/api/permits/" + FileStorageService.FILE_DOWNLOAD_API_ENDPOINT + "/")
+                    .path("/api/employee/" + FileStorageService.FILE_DOWNLOAD_API_ENDPOINT + "/")
                     .path(fileName)
                     .toUriString();
             employee.setFileUrl(fileDownloadUri);
@@ -40,6 +43,46 @@ public class EmployeeController {
         }
 
         return employee;
+    }
+
+
+    //Update file in database API
+    @PutMapping(path = "/update/{id}")
+    public @ResponseBody
+    Employee updateEmployee(@PathVariable Long id,
+                            @RequestParam String name,
+                            @RequestParam(required = false) MultipartFile file) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        if (employeeOptional.isPresent()) {
+            Employee employee = employeeOptional.get();
+            if (name != null) {
+                employee.setName(name);
+            }
+
+            if (file != null) {
+                String fileName = fileStorageService.storeMultipartFile(file, FileStorageService.FILE_DOWNLOAD_API_ENDPOINT, employee.getId() + "");
+                String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                        .path("/api/employee/" + FileStorageService.FILE_DOWNLOAD_API_ENDPOINT + "/")
+                        .path(fileName)
+                        .toUriString();
+                employee.setFileUrl(fileDownloadUri);
+                employeeRepository.save(employee);
+            }
+
+            employeeRepository.save(employee);
+            return employee;
+        } else {
+            throw new MyFileNotFoundException("Resource not found");
+        }
+    }
+
+    // Get all Employee API
+    @GetMapping(path = "/all")
+    public @ResponseBody
+    Iterable<Employee> getAllEmployee(@RequestParam(required = false, defaultValue = "0") int pageNum,
+                                      @RequestParam(required = false, defaultValue = "20") int pageSize) {
+
+        return employeeRepository.findAll(PageRequest.of(pageNum, pageSize));
     }
 
 
